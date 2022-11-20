6a66faadd7a1a7298ecb1f62badb911b40ffc72c
sombriks <sombriks@gmail.com>
2022-11-20 11:54:01 -0300

    project skeleton from spring initializr

---
+++ src/main/java/sample/spring/samplespringmultipledatabases/SampleSpringMultipleDatabasesApplication.java
@@ -0,0 +1,13 @@
+package sample.spring.samplespringmultipledatabases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringMultipleDatabasesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSpringMultipleDatabasesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sample/spring/samplespringmultipledatabases/SampleSpringMultipleDatabasesApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.spring.samplespringmultipledatabases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringMultipleDatabasesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
